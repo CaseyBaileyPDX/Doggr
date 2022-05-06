12d559036cf51be4d3924b0a47fa9abe0c6bd403
Casey Bailey <soniczentropy@gmail.com>
2022-05-06 01:40:10 -0500

    mini file upload from JS working

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,36 @@
 import "dotenv/config";
-import cors from "cors";
-import path from "path";
-
-import fs, { appendFileSync } from "fs";
-import { isAsyncFunction } from "util/types";
-import { assert } from "console";
+import Minio from "minio";
 import express from "express";
-
 import setupRoutes from "./routes";
 
+const _minio = require("minio");
 
+async function minioTest() {
+  const minioClient = new _minio.Client({
+    endPoint: 'localhost',
+    port: 8000,
+    useSSL: false,
+    accessKey: 'minioadmin',
+    secretKey: 'minioadmin',
+  });
+  const metaData = {
+    'Content-Type': 'application/octet-stream',
+    'X-Amz-Meta-Testing': 1234,
+  };
+
+  const file = "/home/kc/workspace/psu/doggr/nginx.conf";
+
+  minioClient.fPutObject("doggr", "nginx.conf", file,  metaData, (err) => {
+    if (err) return console.log(err);
+    console.log('File uploaded successfully.');
+  });
+
+}
 
 async function main() {
 
-
+  await minioTest();
+  return;
   // Don't interfere with Jest/supertest!
   if (process.env.NODE_ENV !== 'test') {
     const app = express();
