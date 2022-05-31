fcb667110e86fd366df6b92614d4bf5a8966dbc2
Casey Bailey <caseybailey@pdx.edu>
2022-05-31 13:58:27 -0500

    Deployer

--- frontend/src/services/HttpService.tsx
+++ frontend/src/services/HttpService.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const external_IP = process.env.BACKEND_API;
+console.log("EXTERNAL_IP IS: ", external_IP);
 
 export const httpClient = axios.create({
   baseURL: external_IP,
